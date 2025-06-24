7654508cb6e1ad78cc5fd1667d3b888fbb3998ac
Rafael Massuci Campos <00001112679212sp@aluno.educacao.sp.gov.br>
2025-06-24 11:14:12 -0300

    mexendo

--- alunos/ver_alunos.php
+++ alunos/ver_alunos.php
@@ -19,7 +19,7 @@ try {
     <title>Lista de Alunos</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Poppins:wght@300;400;500;600;700;800;900&family=JetBrains+Mono:wght@400;500;600&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
-    <link href="ver_alunos.css" rel="stylesheet">
+    <link href="cosmic_ui.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
